ce9fa5c21d1b5a70fcad0e6a591e157ba9996bfd
sksusha8853 <sksusha8853@gmail.com>
2024-05-26 23:18:34 +0530

    create sign-up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { Button, Label, TextInput } from 'flowbite-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div style={{ maxWidth: '36rem' }} className="w-4/5 bg-white p-8 shadow-lg mx-auto mt-5">
+        <form className="flex flex-col gap-5">
+          <div>
+            <Label value="Your Username" />
+            <TextInput
+              type="text"
+              placeholder="Username"
+              id="username"
+            ></TextInput>
+          </div>
+          <div>
+            <Label value="Your Email" />
+            <TextInput
+              type="text"
+              placeholder="Email"
+              id="email"
+            ></TextInput>
+          </div>
+          <div>
+            <Label value="Your Password" />
+            <TextInput
+              type="password"
+              placeholder="Password"
+              id="password"
+            ></TextInput>
+          </div>
+          <Button gradientDuoTone="purpleToBlue" type="submit">
+            Sign Up
+          </Button>
+        </form>
+        <div className="flex gap-2 text-sm mt-2">
+          <span>Already have an account?</span>
+          <Link to="sign-in" className="text-red-500">
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
