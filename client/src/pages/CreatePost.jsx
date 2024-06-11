73241b7622f65c30dac8adc698eed41f5a21f6d9
sksusha8853 <sksusha8853@gmail.com>
2024-06-12 03:27:02 +0530

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,16 @@ import 'react-quill/dist/quill.snow.css';
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import {useNavigate} from 'react-router-dom';
 
 export default function CreatePost() {
     const [file, setFile] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
     const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
+
 
     const handleUploadImage = async () => {
         try {
@@ -50,15 +54,49 @@ export default function CreatePost() {
         }
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const text = await res.text(); // Get the response as text first
+            const data = text ? JSON.parse(text) : {}; // Safely parse the text to JSON
+
+            if (!res.ok) {
+                setPublishError(data.message);
+                return;
+            }
+            else {
+                setPublishError(null);
+                navigate(`/post/${data.slug}`);
+
+
+            }
+        }
+        catch (error) {
+            setPublishError(error.message);
+
+        }
+
+
+    }
+
     return (
         <div className='p-3 max-w-3xl mx-auto min-h-screen'>
             <h1 className='text-center text-3xl my-7 font-semibold'>
                 Create a post
             </h1>
-            <form className='flex flex-col gap-5'>
+            <form className='flex flex-col gap-5' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-5'>
-                    <TextInput id='title' type='text' placeholder='Title' required />
-                    <Select>
+                    <TextInput id='title' type='text' placeholder='Title' required onChange={(e) =>
+                        setFormData({ ...formData, title: e.target.value })} />
+                    <Select onChange={(e) =>
+                        setFormData({ ...formData, category: e.target.value })}>
                         <option value="uncategorized">Select a category</option>
                         <option value="competitiveprogramming">Competitive Programming</option>
                         <option value="dsa">DSA (Data Structures and Algorithms)</option>
@@ -94,15 +132,21 @@ export default function CreatePost() {
                 )}
                 {formData.image && (
                     <img
-                    src={formData.image}
-                    alt='image'
-                    className='w-full h-72 object-cover'
+                        src={formData.image}
+                        alt='image'
+                        className='w-full h-72 object-cover'
                     />
                 )}
-                <ReactQuill theme='snow' placeholder='Write Something...' className='h-72 mb-12' required />
+                <ReactQuill theme='snow' placeholder='Write Something...' className='h-72 mb-12' required
+                    onChange={(value) =>
+                        setFormData({ ...formData, content: value })} />
                 <Button type='submit' gradientDuoTone='purpleToBlue'>
                     Publish
                 </Button>
+                {
+                    publishError &&
+                    <Alert color='failure' className='mt-5'>{publishError}</Alert>
+                }
             </form>
         </div>
     )
--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,9 +1,12 @@
+import Post from '../models/post.model.js';
+import { errHandler } from '../utils/error.js';
+
 export const create = async (req, res, next) => {
     if (!req.user.isAdmin) {
-        return next(errorHandler(403, 'You are not allowed to create a post.'));
+        return next(errHandler(403, 'You are not allowed to create a post.'));
     }
     if (!req.body.title || !req.body.content) {
-        return next(errorHandler(400, 'Please complete all required fields.'));
+        return next(errHandler(400, 'Please complete all required fields.'));
     }
     const slug = req.body.title
         .split(' ')
