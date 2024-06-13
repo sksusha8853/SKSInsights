eb6aef30b6fbade05198722ee3dac25bf5731dfc
sksusha8853 <sksusha8853@gmail.com>
2024-06-14 01:20:40 +0530

    create get users api route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -76,3 +76,46 @@ export const signout = (req, res, next) => {
         next(err);
     }
 };
+
+export const getUsers = async (req, res, next) => {
+    if (!req.user.isAdmin) {
+        return next(errHandler(403, "You are not allowed to see all users."));
+    }
+
+    try {
+        const startIndex = parseInt(req.query.startIndex, 10) || 0;
+        const limit = parseInt(req.query.limit, 10) || 9;
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+        const users = await User.find()
+            .sort({ createdAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+
+        const usersWithoutPassword = users.map((user) => {
+            const { password, ...rest } = user._doc;
+            return rest;
+        });
+
+        const totalUsers = await User.countDocuments();
+
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthUsersCount = await User.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+
+        res.status(200).json({
+            users: usersWithoutPassword,
+            totalUsers,
+            lastMonthUsersCount,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
