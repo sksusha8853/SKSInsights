31fc31e34632feb4a21a3978953e238e26fde786
sksusha8853 <sksusha8853@gmail.com>
2024-06-15 17:49:22 +0530

    add recent arcticles section in the view post page

--- client/src/pages/ViewPost.jsx
+++ client/src/pages/ViewPost.jsx
@@ -2,12 +2,14 @@ import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 export default function ViewPost() {
     const { postSlug } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState([]);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -19,11 +21,10 @@ export default function ViewPost() {
                     setError(data.message || 'Error fetching post');
                     setLoading(false);
                     return;
-                } else {
-                    setPost(data.posts[0]);
-                    setLoading(false);
-                    setError(null);
                 }
+                setPost(data.posts[0]);
+                setLoading(false);
+                setError(null);
             } catch (error) {
                 setError(error.message);
                 setLoading(false);
@@ -31,6 +32,23 @@ export default function ViewPost() {
         };
         fetchPost();
     }, [postSlug]);
+
+    useEffect(() => {
+        const fetchRecentPosts = async () => {
+            try {
+                const res = await fetch(`/api/post/getposts?limit=4`);
+                const data = await res.json();
+                if (res.ok) {
+                    setRecentPosts(data.posts); 
+                } else {
+                    console.log(data.error);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchRecentPosts();
+    }, []);
 
     if (loading) {
         return (
@@ -49,18 +67,14 @@ export default function ViewPost() {
     }
 
     return (
-        <main className='p-6 mx-auto max-w-3xl min-h-screen'>
+        <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
             <h1 className='text-4xl mt-10 font-serif text-center lg:text-5xl'>
                 {post && post.title}
             </h1>
 
-
-
             <div className='flex justify-between py-3 border-b border-slate-500 w-full text-xs mt-4'>
                 <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-                <Link
-                    to={`/search?category=${post && post.category}`}
-                >
+                <Link to={`/search?category=${post && post.category}`}>
                     <Button color='gray' pill size='xxs'>
                         {post && post.category}
                     </Button>
@@ -69,6 +83,7 @@ export default function ViewPost() {
                     {post && (post.content.length / 1000).toFixed(0)} mins read
                 </span>
             </div>
+
             <div className='mt-8 w-full'>
                 <img
                     src={post && post.image}
@@ -78,10 +93,18 @@ export default function ViewPost() {
             </div>
 
             <div
-                className='py-8 w-full post-content border-b border-slate-500 '
+                className='py-8 w-full post-content border-b border-slate-500'
                 dangerouslySetInnerHTML={{ __html: post && post.content }}
             ></div>
-            <CommentSection postId={post._id}/>
+            <CommentSection postId={post._id} />
+
+            <div className='flex flex-col justify-center items-center mb-5'>
+                <h1 className='text-xl mt-5'>Recent articles</h1>
+                <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                    {recentPosts &&
+                        recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+                </div>
+            </div>
         </main>
     );
 }
