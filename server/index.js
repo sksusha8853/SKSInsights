94cd9e4ae38a3809cc3d8e50d25f24e27ddebf60
sksusha8853 <sksusha8853@gmail.com>
2024-06-16 18:44:06 +0530

    updated to render

--- server/index.js
+++ server/index.js
@@ -36,8 +36,8 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoute);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
