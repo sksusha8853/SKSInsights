2267abee3816508864110fe338f77b211a70b1e0
sksusha8853 <sksusha8853@gmail.com>
2024-06-16 18:53:18 +0530

    update index.js to deploy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
@@ -6,46 +6,52 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoute from './routes/comment.route.js';
-
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config();
 
-
-
-mongoose.connect(process.env.MONGODB)
-    .then(() => {
-        console.log('Database is connected');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-const app = express();
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
+app.use(cors()); // Add CORS middleware if needed
 
+// Routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoute);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// Static files
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error!';
     res.status(statusCode).json({
         success: false,
         statusCode,
-        message
-    })
+        message,
+    });
 });
+
+// Database connection and server start
+mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database is connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Database connection error:', err);
+        process.exit(1); // Exit the process if the database connection fails
+    });
